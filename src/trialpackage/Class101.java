817d96c236143aaa1cfcdd2d53d6e41bf896346f
ocakdans <selim.ocakdan@gmail.com>
2020-11-09 23:16:12 +0300

    first from local

---
+++ src/trialpackage/Class101.java
@@ -0,0 +1,13 @@
+package trialpackage;
+
+public class Class101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("git-tohum ekildi");
+		System.out.println("git-tohum sulandý");
+
+	}
+
+}
